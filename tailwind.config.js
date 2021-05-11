c73d260e62bcc7bf4eb667bb8019c0be6e337511
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-05-11 18:08:19 +0200

    jit mode for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,scss,sass}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
