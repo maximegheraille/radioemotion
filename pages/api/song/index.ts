f1ddaefe9b9209596ca856d4630398f5c76d2145
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-05-28 22:13:11 +0200

    added date to current song fetch

--- pages/api/song/index.ts
+++ pages/api/song/index.ts
@@ -9,48 +9,69 @@ const post = (req: NextApiRequest, res: NextApiResponse) => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     });
+    const today = new Date();
+    console.log(
+      `call radioemotion_get_current_song('${
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate()
+      } ${
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+      }')`
+    );
     connection.query(
-      "SELECT * FROM radioemotion_current_song",
+      `call radioemotion_get_current_song('${
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate()
+      } ${
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+      }')`,
       function (error: any, results: any, _fields: any) {
         if (error) {
           console.log(error);
           res.status(500).json({ response: false, error: true });
         }
-        if (results[0].photo === "0") {
-          results[0].photo = "";
+        if (results[0][0].photo === "0") {
+          results[0][0].photo = "";
         } else {
-          results[0].photo = `https://www.radioemotion.be/covers/${results[0].id}.jpg`;
+          results[0][0].photo = `https://www.radioemotion.be/covers/${results[0][0].id}.jpg`;
         }
-        if (results[0].youtube === "0") {
-          results[0].youtube = "";
+        if (results[0][0].youtube === "0") {
+          results[0][0].youtube = "";
         }
         console.log(
-          `call radioemotion_get_voted(${results[0].id}, '${req.connection.remoteAddress}')`
+          `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`
         );
+
         connection.query(
-          `call radioemotion_get_voted(${results[0].id}, '${req.connection.remoteAddress}')`,
+          `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`,
           function (error: any, results2: any, _fields: any) {
-            console.log(results2[0]);
+            console.log(results2);
             console.log(results2[0].length);
             if (error) {
               //throw error;
-              results[0].voted = false;
-              res.status(200).json(results[0]);
+              results[0][0].voted = false;
+              res.status(200).json(results[0][0]);
               console.log("error");
               connection.destroy();
             } else if (results2[0].length > 0) {
               console.log(" voted");
-              results[0].voted = true;
-              res.status(200).json(results[0]);
+              results[0][0].voted = true;
+              res.status(200).json(results[0][0]);
               connection.destroy();
               return;
             } else {
               console.log("not voted");
-              results[0].voted = false;
-              res.status(200).json(results[0]);
+              results[0][0].voted = false;
+              res.status(200).json(results[0][0]);
               connection.destroy();
             }
-            console.log(results[0]);
+            console.log(results[0][0]);
           }
         );
       }
