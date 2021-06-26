0de5f5cf940062b2e7d2458e8a99f76ff5f2d4f9
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-06-26 22:30:24 +0200

    new pages for navigation testing

---
+++ pages/inforegionales.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const inforegionales = () => {
+  return <div></div>;
+};
+
+export default inforegionales;
---
+++ pages/presentation.tsx
@@ -0,0 +1,5 @@
+const presentation = () => {
+  return <div></div>;
+};
+
+export default presentation;
