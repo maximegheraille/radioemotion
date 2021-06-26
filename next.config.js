f1f02ef67b1b6d68fe3e834cd399975ade80bfc1
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-06-26 22:29:14 +0200

    nextjs upgrade + other dependencies upgrade

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const withImages = require("next-images");
-module.exports = withImages({
+// const withImages = require("next-images");
+// module.exports = withImages({
+//   images: {
+//     domains: ["www.radioemotion.be"],
+//   },
+// });
+module.exports = {
   images: {
     domains: ["www.radioemotion.be"],
   },
-});
+};
