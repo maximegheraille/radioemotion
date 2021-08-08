b8f7f520d8e16ba44bc90dc8b48326fe6605bcc4
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-08 18:01:21 +0200

    lastplayed song components

---
+++ components/homepage/lastplayed/Lastplayed.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Song } from "../../../interfaces/song";
+import Card, { outderdivCard } from "../../shared/card/Card";
+
+const Lastplayed = () => {
+  const { data, isLoading, isError } = useQuery("lastPlayed", async () => {
+    const info = await fetch("/api/index/derniers_titres");
+    return info.json();
+  });
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={30}
+        freeMode={true}
+        freeModeSticky
+        watchSlidesVisibility={true}
+        preloadImages={false}
+        resizeObserver={true}
+        lazy={true}
+      >
+        {isLoading ? (
+          <>
+            {[...Array(15)].map((song: Song, index: number) => (
+              <SwiperSlide
+                virtualIndex={index}
+                className={`${outderdivCard}`}
+                key={index}
+              >
+                <Card
+                  song={song}
+                  showTime
+                  isLoading={isLoading}
+                  isError={isError}
+                />
+              </SwiperSlide>
+            ))}
+          </>
+        ) : (
+          <>
+            {data?.map((song: Song) => (
+              <SwiperSlide
+                virtualIndex={song.id}
+                className={`${outderdivCard}`}
+                key={song.id}
+              >
+                <Card
+                  song={song}
+                  showTime
+                  isLoading={isLoading}
+                  isError={isError}
+                />
+              </SwiperSlide>
+            ))}
+          </>
+        )}
+      </Swiper>
+    </>
+  );
+};
+
+export default Lastplayed;
