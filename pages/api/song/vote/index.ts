4a0505f2678f673ed164aec4bdc985b18b461549
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-08 18:01:59 +0200

    api for voting on songs

--- pages/api/song/vote/index.ts
+++ pages/api/song/vote/index.ts
@@ -10,40 +10,28 @@ const post = (req: NextApiRequest, res: NextApiResponse) => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     });
-    console.log("body ", req.body.voted);
-    console.log(
-      `call radioemotion_delete_vote(${req.body.id},'${req.connection.remoteAddress}')`
-    );
+
     if (req.body.voted) {
       connection.query(
-        `call radioemotion_delete_vote(${req.body.id},'${req.connection.remoteAddress}')`,
+        `call radioemotion_delete_vote(${req.body.id},'${req.socket.remoteAddress}')`,
         function (error: any, _results: any, _fields: any) {
           if (error) {
-            console.log(error);
             res.status(500).json({ response: false, error: true });
             return;
           }
-          console.log("voted true");
           res.status(200).json({ success: true });
         }
       );
     } else {
-      console.log(
-        `call radioemotion_add_vote(${req.body.id},'${
-          req.connection.remoteAddress
-        }',${new Date().getDate()})`
-      );
       connection.query(
         `call radioemotion_add_vote(${req.body.id},'${
-          req.connection.remoteAddress
+          req.socket.remoteAddress
         }',${new Date().getDate()})`,
         function (error: any, _results: any, _fields: any) {
           if (error) {
-            console.log(error);
             res.status(500).json({ response: false, error: true });
             return;
           }
-          console.log("voted false");
           res.status(200).json({ success: true });
         }
       );
