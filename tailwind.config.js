e32f589fe1188f66d2c022abee032f54f3ca426c
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-08 17:59:27 +0200

    removed unused txt file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: "jit",
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx,txt}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./styles/**/*.{css,scss,sass}",
   ],
   darkMode: "class", // or 'media' or 'class'
