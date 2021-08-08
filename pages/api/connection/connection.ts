75bd5a22009c5637bb5a445c5e8efc5caa9555f9
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-08 18:02:55 +0200

    api for connection to the db update

--- pages/api/connection/connection.ts
+++ pages/api/connection/connection.ts
@@ -1,20 +1,20 @@
 var mysql = require("mysql");
 
-export const getConnection2 = () => {
-  return mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME2,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
-};
-
 export const getConnection = () => {
   return mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
+};
+
+export const getConnection2 = () => {
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME2,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   });
