7db013def51a63b011ea0ef9ec8706eca29e1166
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-08 18:02:09 +0200

    api for current_song

--- pages/api/song/current_song/index.ts
+++ pages/api/song/current_song/index.ts
@@ -13,15 +13,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       `call radioemotion_get_current_song('${getFullDate()} ${getFullTime()}')`,
       function (error: any, results: any, _fields: any) {
         if (error) {
-          console.log(error);
           res.status(500).json({ response: false, error: true });
           return;
         }
         if (results[0].length === 0) {
-          console.log(
-            `call radioemotion_get_live_emission('${getFullTime()}', '${new Date().getDay()}')`
-          );
-          //  var connection2 = db2;
           connection2.query(
             `call radioemotion_get_live_emission('${getFullTime()}', '${new Date().getDay()}')`,
             function (error: any, emission: any, _fields: any) {
@@ -48,31 +43,23 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
           if (results[0][0].youtube === "0") {
             results[0][0].youtube = "";
           }
-          console.log(
-            `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`
-          );
           connection.query(
-            `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`,
+            `call radioemotion_get_voted(${results[0][0].id}, '${req.socket.remoteAddress}')`,
             function (error: any, results2: any, _fields: any) {
-              console.log(results2);
               if (error) {
                 results[0][0].voted = false;
                 res.status(200).json(results[0][0]);
-                console.log("error");
                 connection.destroy();
               } else if (results2[0].length > 0) {
-                console.log(" voted");
                 results[0][0].voted = true;
                 res.status(200).json(results[0][0]);
                 connection.destroy();
                 return;
               } else {
-                console.log("not voted");
                 results[0][0].voted = false;
                 res.status(200).json(results[0][0]);
                 connection.destroy();
               }
-              console.log(results[0][0]);
             }
           );
         }
@@ -84,102 +71,3 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     connection.destroy();
   }
 }
-
-// var mysql = require("mysql");
-// const post = (req: NextApiRequest, res: NextApiResponse) => {
-//   try {
-//     var connection = mysql.createConnection({
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       database: process.env.DB_NAME,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//     });
-//     //  var connection = db.connect();
-
-//     connection.query(
-//       `call radioemotion_get_current_song('${getFullDate()} ${getFullTime()}')`,
-//       function (error: any, results: any, _fields: any) {
-//         if (error) {
-//           console.log(error);
-//           res.status(500).json({ response: false, error: true });
-//           return;
-//         }
-//         if (results[0].length === 0) {
-//           var connection2 = mysql.createConnection({
-//             host: process.env.DB_HOST,
-//             port: process.env.DB_PORT,
-//             database: process.env.DB_NAME2,
-//             user: process.env.DB_USER,
-//             password: process.env.DB_PASSWORD,
-//           });
-//           console.log(
-//             `call radioemotion_get_live_emission('${getFullTime()}', '${new Date().getDay()}')`
-//           );
-//           //  var connection2 = db2;
-//           connection2.query(
-//             `call radioemotion_get_live_emission('${getFullTime()}', '${new Date().getDay()}')`,
-//             function (error: any, emission: any, _fields: any) {
-//               if (error) {
-//                 res.status(500).json(error);
-//                 connection2.destroy();
-//                 return;
-//               }
-//               res.status(200).json({
-//                 artiste: emission[0][0].EMISSION,
-//                 titre: emission[0][0].Nom,
-//               });
-//               connection2.destroy();
-//             }
-//           );
-//         } else {
-//           // verify if image for song is available
-//           if (results[0][0].photo === "0") {
-//             results[0][0].photo = "";
-//           } else {
-//             results[0][0].photo = `https://www.radioemotion.be/covers/${results[0][0].id}.jpg`;
-//           }
-
-//           // verify if there is a youtube link
-//           if (results[0][0].youtube === "0") {
-//             results[0][0].youtube = "";
-//           }
-
-//           console.log(
-//             `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`
-//           );
-//           connection.query(
-//             `call radioemotion_get_voted(${results[0][0].id}, '${req.connection.remoteAddress}')`,
-//             function (error: any, results2: any, _fields: any) {
-//               console.log(results2);
-//               if (error) {
-//                 results[0][0].voted = false;
-//                 res.status(200).json(results[0][0]);
-//                 console.log("error");
-//                 connection.destroy();
-//               } else if (results2[0].length > 0) {
-//                 console.log(" voted");
-//                 results[0][0].voted = true;
-//                 res.status(200).json(results[0][0]);
-//                 connection.destroy();
-//                 return;
-//               } else {
-//                 console.log("not voted");
-//                 results[0][0].voted = false;
-//                 res.status(200).json(results[0][0]);
-//                 connection.destroy();
-//               }
-//               console.log(results[0][0]);
-//             }
-//           );
-//         }
-//       }
-//     );
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ response: false, error: true });
-//     connection.destroy();
-//   }
-// };
-
-// export default post;
