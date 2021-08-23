9e5ade9071e30254b6f7c83d503250db40499311
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-08-23 19:16:45 +0200

    remove reactquerydevtools

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import LazyLoad from "react-lazyload";
+import Infos from "../components/homepage/infos/Infos";
+import Agenda from "../components/homepage/agenda/Agenda";
 
 // import Lastplayed from "../components/homepage/lastPlayed/Lastplayed";
 // import Title from "../components/shared/title/Title";
@@ -22,7 +24,7 @@ const Emissions = dynamic(
 );
 const IndexPage = () => {
   return (
-    <div>
+    <div className="">
       <div className="">
         <div className="mx-auto pt-7 rounded-md z-0 md:w-10/12 lg:w-6/12 2xl:w-6/12">
           <Carousel />
@@ -30,16 +32,32 @@ const IndexPage = () => {
         <Title title={"DERNIERS TITRES DIFFUSES"} className="" />
         <Lastplayed />
         <div className="lg:flex place-content-between">
-          <div className="w-full lg:w-auto">
+          <div className="lg:w-[70%]">
             <Title title="DERNIERES NOUVEAUTES" />
-            <div className="w-full">
+            <div className="">
               <NewSongs />
             </div>
           </div>
-          <div className="lg:w-3/5">
+          <div className="lg:w-[30%]">
             <Title title="EMISSIONS DU JOUR" />
             <LazyLoad>
               <Emissions />
+            </LazyLoad>
+          </div>
+        </div>
+        <div className="lg:flex place-content-between">
+          <div className="lg:w-[70%]">
+            <Title title="INFO REGIONALE" />
+            <div className="">
+              <LazyLoad>
+                <Infos />
+              </LazyLoad>
+            </div>
+          </div>
+          <div className="lg:w-[30%]">
+            <Title title="AGENDA" />
+            <LazyLoad>
+              <Agenda />
             </LazyLoad>
           </div>
         </div>
