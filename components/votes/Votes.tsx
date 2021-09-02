3f94a83a7b77beaf4c5a3666bfd62e1e26d5f35d
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-02 09:45:04 +0200

    votes page

---
+++ components/votes/Votes.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { Song } from "../../interfaces/song";
+import Card, { outderdivCard } from "../shared/card/Card";
+
+const Votes = () => {
+  const { data, isLoading, isError } = useQuery(
+    `votes${new Date().getFullYear()}`,
+    async () => {
+      const info = await fetch("/api/votes");
+      return info.json();
+    },
+    {
+      refetchIntervalInBackground: true,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  return (
+    <div>
+      {isLoading || isError ? (
+        <div className="flex flex-wrap justify-center space-x-4">
+          {[...Array(42)].map((song: Song, index: number) => (
+            <div className={`${outderdivCard} mb-5 ml-4`} key={index}>
+              <Card
+                song={song}
+                isLoading={isLoading}
+                isError={isError}
+                key={index}
+                number={true}
+                component="nothing"
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex-wrap flex justify-center space-x-4">
+          {data?.map((song: Song, index: number) => (
+            <div className={`${outderdivCard} mb-5 ml-4`} key={index}>
+              <Card
+                song={song}
+                isLoading={isLoading}
+                isError={isError}
+                number={true}
+                component="votes"
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Votes;
---
+++ pages/votes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Votes from "../components/votes/Votes";
+
+const votes2021 = () => {
+  return (
+    <div>
+      <Votes />
+    </div>
+  );
+};
+
+export default votes2021;
