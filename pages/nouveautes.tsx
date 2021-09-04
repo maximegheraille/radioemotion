3b13f0fed03fdd30eac8ad374c75fcf1bedccc12
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-04 21:18:24 +0200

    nouveautes page

--- pages/nouveautes.tsx
+++ pages/nouveautes.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nouveautes from "../components/nouveautes/Nouveautes";
 import LazyLoad from "react-lazyload";
-
+import nouveautesban from "../public/images/bannieres/nouveautes.jpg";
+import Banniere from "../components/shared/banniere/Banniere";
 const nouveautes = () => {
   return (
     <div>
+      <LazyLoad>
+        <Banniere image={nouveautesban} />
+      </LazyLoad>
       <LazyLoad>
         <Nouveautes />
       </LazyLoad>
