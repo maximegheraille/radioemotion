4e6c9867e4f20ee421674a88e667da47beb443be
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-04 21:17:30 +0200

    removed unused code

--- pages/top30.tsx
+++ pages/top30.tsx
@@ -1,27 +1,23 @@
-//import { GetStaticProps, InferGetServerSidePropsType } from "next";
 import React from "react";
 import LazyLoad from "react-lazyload";
+import Banniere from "../components/shared/banniere/Banniere";
 import Top30 from "../components/top30/Top30";
-//import { server } from "../config/nextjs";
-//import { Song } from "../interfaces/song";
-const top30 = (/*{
-  top30,
-}: InferGetServerSidePropsType<typeof getServerSideProps>*/) => {
+import bantop30 from "../public/images/bannieres/top30.jpg";
+
+const top30 = () => {
   return (
     <div>
       <LazyLoad>
-        <Top30 /*top30={top30} */ />
+        <Banniere image={bantop30} />
+      </LazyLoad>
+      <LazyLoad>
+        <Top30 />
       </LazyLoad>
     </div>
   );
 };
 
 export default top30;
-// export const getServerSideProps: GetStaticProps = async (_context) => {
-//   const res = await fetch(`${server}/api/top30`);
-//   const top30: Song[] = await res.json();
-//   console.log(top30);
-//   console.log("ss");
 
 //   return {
 //     props: {
