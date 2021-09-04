cd77a97a44c4394e388877e4726152247145a677
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-04 21:15:33 +0200

    sizing

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -7,7 +7,7 @@ const Player = () => {
 
   return (
     <div className="fixed bottom-0 bg-[#2d2180] w-full h-12 z-50">
-      <div className="w-full flex mx-auto h-full max-h-full place-content-between px-1 lg:px-8">
+      <div className="w-full flex mx-auto h-full max-h-full place-content-between px-1 lg:px-8 border-t-gray-600 border-t-2">
         <PlayerInfos />
         <PlayerButton setVolume={setVolume} volume={volume} />
         <PlayerLinks setVolume={setVolume} volume={volume} />
