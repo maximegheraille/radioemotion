8015757213c894a1cf681db958488b1507621208
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-19 17:58:12 +0200

    try catch

--- config/context/initialTheme.ts
+++ config/context/initialTheme.ts
@@ -1,15 +1,19 @@
 export const getInitialTheme = () => {
-  if (
-    !("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  ) {
-    localStorage.theme = "dark";
-    return true;
-  } else if (localStorage.theme === "dark") {
-    localStorage.theme = "dark";
-    return true;
-  } else {
-    localStorage.theme = "white";
+  try {
+    if (
+      !("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      localStorage.theme = "dark";
+      return true;
+    } else if (localStorage.theme === "dark") {
+      localStorage.theme = "dark";
+      return true;
+    } else {
+      localStorage.theme = "white";
+      return false;
+    }
+  } catch {
     return false;
   }
 };
