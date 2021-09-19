b708626a7cd4cd85516c1e07093b730a1ae2bfb8
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-19 17:56:49 +0200

    type change

--- components/homepage/newSongs/NewSongs.tsx
+++ components/homepage/newSongs/NewSongs.tsx
@@ -85,7 +85,7 @@ const NewSongs = () => {
                 <Card
                   key={index}
                   song={song}
-                  component="nothing"
+                  component="newSongs"
                   isLoading={isLoading}
                   isError={isError}
                 />
@@ -111,74 +111,6 @@ const NewSongs = () => {
           </div>
         </>
       )}
-      {/* <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={30}
-        freeMode={true}
-        freeModeSticky
-        watchSlidesVisibility={true}
-        preloadImages={false}
-        resizeObserver={true}
-        lazy={true}
-        className="lg:hidden"
-      >
-        {isLoading || isError ? (
-          <>
-            {[...Array(10)].map((song: Song, index: number) => (
-              <SwiperSlide
-                virtualIndex={index}
-                className={`${outderdivCard}`}
-                key={index}
-              >
-                <Card
-                  key={index}
-                  song={song}
-                  showTime
-                  component="nothing"
-                  isLoading={isLoading}
-                  isError={isError}
-                />
-              </SwiperSlide>
-            ))}
-          </>
-        ) : (
-          <>
-            {[...Array(10)].map((song: Song, index: number) => (
-              <SwiperSlide
-                virtualIndex={song.id}
-                className={`${outderdivCard}`}
-                key={index}
-              >
-                <Card
-                  key={index}
-                  song={song}
-                  component="nothing"
-                  isLoading={isLoading}
-                  isError={isError}
-                />
-              </SwiperSlide>
-            ))}
-          </>
-        )}
-      </Swiper>
-
-      <div className="flex-wrap w-full hidden lg:flex">
-        {data?.map((song: Song, index: number) => (
-          <div
-            className={`${outderdivCard} mr-[30px] mb-5 ${
-              index > 5 && "lg:hidden xl:block"
-            } ${index > 7 && "xl:hidden 2xl:block"}`}
-            key={index}
-          >
-            <Card
-              song={song}
-              isLoading={isLoading}
-              isError={isError}
-              component="newSongs"
-            />
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };
