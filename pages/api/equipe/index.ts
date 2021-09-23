a5e305fc0b58ff9543257e0796fd8d1814686e69
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-09-23 21:09:46 +0200

    equipe page

---
+++ pages/api/equipe/index.ts
@@ -0,0 +1,32 @@
+import { Equipe } from "./../../../interfaces/equipe";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getConnection2 } from "../connection/connection";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const connection = getConnection2();
+  if (req.method === "GET") {
+    connection.query(
+      `select * from radioemotion_get_equipe`,
+      async (err: any, rows: Equipe[], _fields: any) => {
+        if (err) {
+          res.status(500).json({ response: false, error: true });
+          connection.destroy();
+          console.log(err);
+          return;
+        }
+        rows.forEach((person: Equipe) => {
+          person.photo = `https://www.radioemotion.be${person.photo}.jpg`;
+        });
+        res.status(200).json(rows);
+        connection.destroy();
+      }
+    );
+  } else {
+    // Handle any other HTTP method
+    res.status(500).json({ response: false, error: true });
+    connection.destroy();
+  }
+}
