4a4f9caff8e2fc6efdd280970079426ea434dbb2
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-10-01 22:23:45 +0200

    new props

--- components/shared/title/Title.tsx
+++ components/shared/title/Title.tsx
@@ -2,14 +2,20 @@ import React from "react";
 
 interface TitleProps {
   title: String;
+  article?: boolean;
   className?: string;
 }
 
-const Title = ({ title, className = "" }: TitleProps) => {
+const Title = ({ title, className = "", article }: TitleProps) => {
+  {
+    console.log(article);
+  }
   return (
     <div>
       <h1
-        className={`text-2xl font-bold pb-6 pt-4 lg:pt-10 text-black dark:text-white ${className}`}
+        className={`text-2xl font-bold pb-6 ${
+          article === true ? className : "pt-4 lg:pt-10"
+        } text-black dark:text-white ${className}`}
       >
         {title}
       </h1>
