ecb842b37a1da2b1a9a5c5a883843bdfb4d2d3f4
maximegheraille <32335849+maximegheraille@users.noreply.github.com>
2021-10-03 15:40:52 +0200

    sizing for mobile

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -10,7 +10,7 @@ const Player = () => {
   }, []);
   return (
     <div
-      className={`fixed bottom-0 px-1 lg:px-0 bg-[#2d2180] w-full h-[4.2rem] lg:h-12 z-50`}
+      className={`fixed bottom-0 px-4 lg:px-0 bg-[#2d2180] w-full h-[4.2rem] lg:h-12 z-50`}
     >
       <div className="w-full flex mx-auto h-full max-h-full place-content-between lg:place-self-auto px-1 lg:px-8 border-t-gray-600 border-t-2">
         <PlayerInfos />
