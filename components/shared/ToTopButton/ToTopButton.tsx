5d4c47499a92f95e13db92f33b723fed780ea634
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-10-14 19:09:56 +0200

    go to top of screen button

---
+++ components/shared/ToTopButton/ToTopButton.tsx
@@ -0,0 +1,26 @@
+import {
+  faArrowAltCircleUp,
+  faCaretSquareUp,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const ToTopButton = () => {
+  return (
+    <button
+      className="fixed left-[90vw] top-[90vh]"
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+      aria-label="Remonter au haut de la page"
+    >
+      <FontAwesomeIcon
+        icon={faArrowAltCircleUp}
+        className="text-[#bfbfbf]"
+        size="2x"
+      />
+    </button>
+  );
+};
+
+export default ToTopButton;
