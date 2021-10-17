a91e5dfe5273217e41e5fd7b427729933ad491bb
Maxime Ghéraille <32335849+maximegheraille@users.noreply.github.com>
2021-10-17 15:58:42 +0200

    make app a PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,67 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/images/favicon.ico" />
+          {/* manifest */}
+          <link rel="manifest" href="/manifest.json" />
+
+          {/*  icons meta tags */}
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-config"
+            content="/images/icons/apple-touch-icons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* apple-touch-icons */}
+          <link
+            rel="shortcut icon"
+            href="/images/icons/favicon.ico"
+            type="image/x-icon"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/images/icons/apple-touch-icons/apple-touch-icon.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/icons/apple-touch-icons/apple-touch-icon-180x180.png"
+          />
         </Head>
         <body>
           <Main />
