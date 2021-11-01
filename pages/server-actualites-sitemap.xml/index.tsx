10527e8eb172963f3a2422543f5529308c4152fe
Maxime Ghéraille <maxime.gheraille@hotmail.com>
2021-11-01 13:49:41 +0100

    url for website

--- pages/server-actualites-sitemap.xml/index.tsx
+++ pages/server-actualites-sitemap.xml/index.tsx
@@ -1,12 +1,13 @@
 import { GetServerSideProps } from "next";
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 import { server } from "../../config/nextjs";
+import { siteUrl } from "../../next-sitemap.config";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const response = await fetch(`${server}/api/actualites/all`);
   const capsules: any[] = await response.json();
   const fields: ISitemapField[] = capsules.map((info) => ({
-    loc: `${server}/actualites/${info.id}`,
+    loc: `${siteUrl}/actualites/${info.id}`,
     id: new Date().toISOString(),
   }));
 
--- pages/server-agenda-sitemap.xml/index.tsx
+++ pages/server-agenda-sitemap.xml/index.tsx
@@ -1,13 +1,14 @@
 import { GetServerSideProps } from "next";
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 import { server } from "../../config/nextjs";
+import { siteUrl } from "../../next-sitemap.config";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const response = await fetch(`${server}/api/agenda/all`);
   const capsules: any[] = await response.json();
 
   const fields: ISitemapField[] = capsules.map((agenda) => ({
-    loc: `${server}/agenda/${agenda.id}`,
+    loc: `${siteUrl}/agenda/${agenda.id}`,
     id: new Date().toISOString(),
   }));
 
