28c9473fbfb4f226f7d7536fa84e892e5a6389d2
Maxime Ghéraille <maxime.gheraille@hotmail.com>
2021-11-13 14:08:32 +0100

    pwa bug fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPWA = require("next-pwa");
-
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    scope: "/",
+    register: true,
+    buildExcludes: [/middleware-manifest\.json$/],
   },
   images: {
     domains: ["covers.radioemotion.be"],
