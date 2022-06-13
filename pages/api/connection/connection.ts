43f0b1fb16a019ab28c179471e151044ca2aa76e
Maxime Ghéraille <maxime.gheraille@hotmail.com>
2022-06-13 18:40:01 +0200

    connection try catch

--- pages/api/connection/connection.ts
+++ pages/api/connection/connection.ts
@@ -1,21 +1,31 @@
 var mysql = require("mysql");
 
 export const getConnection = () => {
-  return mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
+  try {
+    return mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+  } catch {
+    console.log("return connection 1");
+    return;
+  }
 };
 
 export const getConnection2 = () => {
-  return mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME2,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
+  try {
+    return mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME2,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+  } catch {
+    console.log("return connection 2");
+    return;
+  }
 };
